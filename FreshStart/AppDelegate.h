999e42bdc11434579fcad29e5c537a4151d996d8
Muddassar Mubeen <Muddassar@Muddassars-MacBook-Pro.local>
2014-12-19 23:52:28 -0500

    Initial Commit

---
+++ FreshStart/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FreshStart
+//
+//  Created by Muddassar Mubeen on 12/19/14.
+//  Copyright (c) 2014 Muddassar Mubeen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FreshStart/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FreshStart
+//
+//  Created by Muddassar Mubeen on 12/19/14.
+//  Copyright (c) 2014 Muddassar Mubeen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
